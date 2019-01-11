ebe114f972598add39a53d30dfc3ee01da2561c5
timic <timotej.cerar@student.um.si>
2019-01-11 18:22:51 +0100

    Posodobitev importov

--- src/main/java/si/feri/um/controller/RestaurantController.java
+++ src/main/java/si/feri/um/controller/RestaurantController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import si.feri.um.repositories.RestaurantRepository;
 import si.feri.um.vao.Restaurant;
-import si.feri.um.vao.User;
 
 import java.util.List;
 
